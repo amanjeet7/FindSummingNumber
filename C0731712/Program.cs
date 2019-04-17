cc2f9e4d41403cfff2d5da16fed1b47ffbc0b878
visualstudio <visualstudio@122-PC21>
2019-04-17 09:56:31 -0400

    final commi for SummingNumber

--- C0731712/Program.cs
+++ C0731712/Program.cs
@@ -13,24 +13,48 @@ namespace C0731712
         {
             new GenerateList().Run(57);
         }
-    }
-    class GenerateList
-    {
-        const int LEN = 100;
-        public ArrayList ListQ;
-        int AddUpToNumber = 0;
-        public void Run(int AddUpToNumber)
 
+        class GenerateList
         {
-            Random r = new Random();
-            ListQ = new ArrayList();
-            for (int i = 0; i < LEN; i++)
-            {
-                ListQ.Add(r.Next(100));
-            }
-            this.findTwoNumbersThatAddUpTo();
-        }
+            const int LEN = 100;
+            public ArrayList ListQ;
+            int AddUpToNumber = 0;
+            public void Run(int AddUpToNumber)
 
+
+            {
+                Random r = new Random();
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+                }
+                this.findTwoNumbersThatAddUpTo();
+            }
+
+            public void findTwoNumbersThatAddUpTo()
+            {
+                foreach (int num1 in ListQ)
+                {
+                    int x, y;
+                    x = num1;
+                    foreach (int num2 in ListQ)
+                    {
+                        y = num2;
+                        if (x + y == 57)
+                        {
+                            Console.WriteLine("the numbers that add up tp 57 is== " + x + " " + y);
+                        }
+                    }
+
+
+                }
+                Console.ReadLine();
+            }
+
+
+        }
     }
 }
-}
+
+
